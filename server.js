38bd7212b35ca9409e318b9182a768f0d8e40327
onuralpArlsn <onuralparslan97@gmail.com>
2026-01-03 10:46:47 +0300

    portset

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });

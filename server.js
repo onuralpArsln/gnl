2a8b222e4b6274862a09a04ac8c31d83f4f80408
onuralpArlsn <onuralparslan97@gmail.com>
2026-01-03 10:55:37 +0300

    server correction

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const server = http.createServer((req, res) => {
 });
 
 const PORT = 3001;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
     console.log(`Server is running on port ${PORT}`);
 });
